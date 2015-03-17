7069d8af43c896d8d4e5b41940da3d1ff2f45548
klaus <claudiogonzalezdai@gmail.com>
2015-03-17 19:45:59 +0100

    added exercise 7

---
+++ 7.httpclient.js
@@ -0,0 +1,17 @@
+var http = require('http');                      
+
+http.get(process.argv[2], function(res) {
+  res.setEncoding('utf8');
+  res.on("data", function(data) {
+    console.log(data);
+  });
+  res.on('error', function(e) {
+    console.log(e.message);
+  });
+});
+
+// http.get(process.argv[2], function (response) {
+//  response.setEncoding('utf8');
+//  response.on('data', console.log);
+//  response.on('error', console.error);
+// });
